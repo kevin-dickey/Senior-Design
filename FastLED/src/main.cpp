746357f7babb1bdd85a66b919bde982cc7b7ae95
kdickey <kdickey@iastate.edu>
2024-03-25 10:15:26 -0500

    initial commit

--- FastLED/src/main.cpp
+++ FastLED/src/main.cpp
@@ -1,73 +1,3 @@
-#include <Arduino.h>
-#define FASTLED_INTERNAL
-// #include <FastLED.h>
-
-
-// #define NUM_LEDS  256
-// #define LED_PIN   22 
-
-// CRGB leds[NUM_LEDS] = {0};  // Initialize the array of leds
-
-// // FramesPerSecond
-// //
-// // Tracks a weighted average to smooth out the values that it calcs as the simple reciprocal
-// // of the amount of time taken specified by the caller. So 1/3 of a second is 3 fps, and it
-// // will take up to 10 frames or so to stabilize on that value.
-
-// double FramesPerSecond(double seconds) {
-//   static double framesPerSecond;
-//   framesPerSecond = (framesPerSecond * .9) + (1.0 / seconds * .1);
-//   return framesPerSecond;
-// }
-
-// void setup() {
-//   pinMode(LED_BUILTIN, OUTPUT);
-//   pinMode(LED_PIN, OUTPUT);
-
-//   Serial.begin(115200);
-//   while (!Serial) {}
-//   Serial.println("ESP32 Startup");
-
-//   FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS);    // Add our strip to the FastLED library
-//   FastLED.setBrightness(16);
-
-// }
-
-// void loop() {
-//   bool bLED = 0;
-//   double fps = 0;
-
-//   uint8_t hue = 0;
-//   const uint8_t deltaHue = 8;
-//   const uint8_t hueDensity = 8;
-
-//   for (;;)
-//   {
-//     bLED = !bLED;
-//     digitalWrite(LED_BUILTIN, bLED);
-
-//     double dStart = millis() / 1000.0;
-
-//     // for (int i = 0; i < NUM_LEDS; i += 5)
-//     // {
-//     //   leds[i] = CHSV(hue + (i * hueDensity), 255, 255);
-//     // }
-//     fill_rainbow(leds, NUM_LEDS, hue += hueDensity, deltaHue);
-
-//     FastLED.show();
-//     Serial.println(fps);
-//     delay(100);
-  
-//     double dEnd = millis() / 1000.0;
-//     fps = FramesPerSecond(dEnd - dStart);
-//   }
-// }
-
-
-/// @file    XYMatrix.ino
-/// @brief   Demonstrates how to use an XY position helper function with a 2D matrix
-/// @example XYMatrix.ino
-
 #include <FastLED.h>
 
 #define LED_PIN  22
@@ -246,6 +176,7 @@ uint16_t XYsafe( uint8_t x, uint8_t y)
   return XY(x,y);
 }
 
+
 // Demo that USES "XY" follows code below
 void DrawOneFrame( uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
 {
@@ -259,6 +190,7 @@ void DrawOneFrame( uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
     }
   }
 }
+
 
 void loop()
 {
@@ -287,3 +219,76 @@ void setup() {
   FastLED.setBrightness( BRIGHTNESS );
   // randomSeed(analogRead(0));
 }
+
+
+
+
+// #include <Arduino.h>
+// #define FASTLED_INTERNAL
+// // #include <FastLED.h>
+
+
+// // #define NUM_LEDS  256
+// // #define LED_PIN   22 
+
+// // CRGB leds[NUM_LEDS] = {0};  // Initialize the array of leds
+
+// // // FramesPerSecond
+// // //
+// // // Tracks a weighted average to smooth out the values that it calcs as the simple reciprocal
+// // // of the amount of time taken specified by the caller. So 1/3 of a second is 3 fps, and it
+// // // will take up to 10 frames or so to stabilize on that value.
+
+// // double FramesPerSecond(double seconds) {
+// //   static double framesPerSecond;
+// //   framesPerSecond = (framesPerSecond * .9) + (1.0 / seconds * .1);
+// //   return framesPerSecond;
+// // }
+
+// // void setup() {
+// //   pinMode(LED_BUILTIN, OUTPUT);
+// //   pinMode(LED_PIN, OUTPUT);
+
+// //   Serial.begin(115200);
+// //   while (!Serial) {}
+// //   Serial.println("ESP32 Startup");
+
+// //   FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS);    // Add our strip to the FastLED library
+// //   FastLED.setBrightness(16);
+
+// // }
+
+// // void loop() {
+// //   bool bLED = 0;
+// //   double fps = 0;
+
+// //   uint8_t hue = 0;
+// //   const uint8_t deltaHue = 8;
+// //   const uint8_t hueDensity = 8;
+
+// //   for (;;)
+// //   {
+// //     bLED = !bLED;
+// //     digitalWrite(LED_BUILTIN, bLED);
+
+// //     double dStart = millis() / 1000.0;
+
+// //     // for (int i = 0; i < NUM_LEDS; i += 5)
+// //     // {
+// //     //   leds[i] = CHSV(hue + (i * hueDensity), 255, 255);
+// //     // }
+// //     fill_rainbow(leds, NUM_LEDS, hue += hueDensity, deltaHue);
+
+// //     FastLED.show();
+// //     Serial.println(fps);
+// //     delay(100);
+  
+// //     double dEnd = millis() / 1000.0;
+// //     fps = FramesPerSecond(dEnd - dStart);
+// //   }
+// // }
+
+
+// /// @file    XYMatrix.ino
+// /// @brief   Demonstrates how to use an XY position helper function with a 2D matrix
+// /// @example XYMatrix.ino
