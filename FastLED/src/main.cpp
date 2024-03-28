b8dc8bbf9b54fd9c62da6695b4e1edb05e0e2f84
kdickey <kdickey@iastate.edu>
2024-03-27 20:22:17 -0500

    fixed most compiler errors

--- FastLED/src/main.cpp
+++ FastLED/src/main.cpp
@@ -1,295 +1,165 @@
+
 #include <FastLED.h>
 
-#define LED_PIN  22
-
+#define LED_PIN     22
+#define NUM_LEDS    256
+#define BRIGHTNESS  8
+#define LED_TYPE    WS2812B
 #define COLOR_ORDER GRB
-#define CHIPSET     WS2811
 
-#define BRIGHTNESS 8 // max is 64 (?) maybe 256 idk
-// if you set the brightness to 1, it only shows red stripes! DO NOT DO THIS WITHOUT A POWER SUPPLY CONNECTED (byebye usb)
-// even brightness of 2 is able to show the colors quite well, but probably missing some saturation?
-
-
-// Helper functions for an two-dimensional XY matrix of pixels.
-// Simple 2-D demo code is included as well.
-//
-//     XY(x,y) takes x and y coordinates and returns an LED index number,
-//             for use like this:  leds[ XY(x,y) ] == CRGB::Red;
-//             No error checking is performed on the ranges of x and y.
-//
-//     XYsafe(x,y) takes x and y coordinates and returns an LED index number,
-//             for use like this:  leds[ XYsafe(x,y) ] == CRGB::Red;
-//             Error checking IS performed on the ranges of x and y, and an
-//             index of "-1" is returned.  Special instructions below
-//             explain how to use this without having to do your own error
-//             checking every time you use this function.  
-//             This is a slightly more advanced technique, and 
-//             it REQUIRES SPECIAL ADDITIONAL setup, described below.
+void rippleEffect();
+int calculateDistance(int x1, int y1, int x2, int y2);
+int scaleBrightness(int distance, int rippleCounter);
 
 
-// Params for width and height
-const uint8_t kMatrixWidth = 16;
-const uint8_t kMatrixHeight = 16;
+CRGB leds[NUM_LEDS];
 
-// Param for different pixel layouts
-const bool    kMatrixSerpentineLayout = true;
-const bool    kMatrixVertical = false;
+void setup() {
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+}
 
-// Set 'kMatrixSerpentineLayout' to false if your pixels are 
-// laid out all running the same way, like this:
-//
-//     0 >  1 >  2 >  3 >  4
-//                         |
-//     .----<----<----<----'
-//     |
-//     5 >  6 >  7 >  8 >  9
-//                         |
-//     .----<----<----<----'
-//     |
-//    10 > 11 > 12 > 13 > 14
-//                         |
-//     .----<----<----<----'
-//     |
-//    15 > 16 > 17 > 18 > 19
-//
-// Set 'kMatrixSerpentineLayout' to true if your pixels are 
-// laid out back-and-forth, like this:
-//
-//     0 >  1 >  2 >  3 >  4
-//                         |
-//                         |
-//     9 <  8 <  7 <  6 <  5
-//     |
-//     |
-//    10 > 11 > 12 > 13 > 14
-//                        |
-//                        |
-//    19 < 18 < 17 < 16 < 15
-//
-// Bonus vocabulary word: anything that goes one way 
-// in one row, and then backwards in the next row, and so on
-// is call "boustrophedon", meaning "as the ox plows."
+void loop() {
+  rippleEffect();
+  FastLED.show();
+  delay(50);  // Adjust delay for speed of the ripple effect
+}
+
+void rippleEffect() {
+  static int rippleCounter = 0;
+  int center_x = NUM_LEDS / 2;
+  int center_y = NUM_LEDS / 2;
+
+  for (int x = 0; x < NUM_LEDS; x++) {
+    for (int y = 0; y < NUM_LEDS; y++) {
+      int distance = calculateDistance(center_x, center_y, x, y);
+      int brightness = scaleBrightness(distance, rippleCounter);
+      leds[ XY(x, y)] = CRGB(brightness, 0, 0);  // just red
+    }
+  }
+
+  rippleCounter++;
+}
+
+int calculateDistance(int x1, int y1, int x2, int y2) {
+  float dx = abs(x2 - x1);
+  float dy = abs(y2 - y1);
+  return sqrt(dx * dx + dy * dy);
+}
+
+int scaleBrightness(int distance, int rippleCounter) {
+  int delta = abs(rippleCounter - distance);
+  int maxDistance = NUM_LEDS / 2;
+  int brightness = map(delta, 0, maxDistance, 255, 0);
+  return brightness > 255 ? 0 : brightness; // Ensures brightness does not exceed 255 or go below 0
+}
 
 
-// This function will return the right 'led index number' for 
-// a given set of X and Y coordinates on your matrix.  
-// IT DOES NOT CHECK THE COORDINATE BOUNDARIES.  
-// That's up to you.  Don't pass it bogus values.
-//
-// Use the "XY" function like this:
-//
-//    for( uint8_t x = 0; x < kMatrixWidth; x++) {
-//      for( uint8_t y = 0; y < kMatrixHeight; y++) {
-//      
-//        // Here's the x, y to 'led index' in action: 
-//        leds[ XY( x, y) ] = CHSV( random8(), 255, 255);
-//      
-//      }
-//    }
-//
-//
-uint16_t XY( uint8_t x, uint8_t y)
-{
-  uint16_t i;
+// #include <FastLED.h>
+
+// #define LED_PIN  22
+
+// #define COLOR_ORDER GRB
+// #define CHIPSET     WS2811
+
+// #define BRIGHTNESS 8 // max is 64 (?) maybe 256 idk
+// // if you set the brightness to 1, it only shows red stripes! DO NOT DO THIS WITHOUT A POWER SUPPLY CONNECTED (byebye usb)
+// // even brightness of 2 is able to show the colors quite well, but probably missing some saturation?
+
+// // Params for width and height
+// const uint8_t kMatrixWidth = 16;
+// const uint8_t kMatrixHeight = 16;
+
+// // Param for different pixel layouts
+// const bool    kMatrixSerpentineLayout = true;
+// const bool    kMatrixVertical = false;
+
+// uint16_t XY( uint8_t x, uint8_t y)
+// {
+//   uint16_t i;
   
-  if( kMatrixSerpentineLayout == false) {
-    if (kMatrixVertical == false) {
-      i = (y * kMatrixWidth) + x;
-    } else {
-      i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
-    }
-  }
+//   if( kMatrixSerpentineLayout == false) {
+//     if (kMatrixVertical == false) {
+//       i = (y * kMatrixWidth) + x;
+//     } else {
+//       i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
+//     }
+//   }
 
-  if( kMatrixSerpentineLayout == true) {
-    if (kMatrixVertical == false) {
-      if( y & 0x01) {
-        // Odd rows run backwards
-        uint8_t reverseX = (kMatrixWidth - 1) - x;
-        i = (y * kMatrixWidth) + reverseX;
-      } else {
-        // Even rows run forwards
-        i = (y * kMatrixWidth) + x;
-      }
-    } else { // vertical positioning
-      if ( x & 0x01) {
-        i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
-      } else {
-        i = kMatrixHeight * (kMatrixWidth - x) - (y+1);
-      }
-    }
-  }
+//   if( kMatrixSerpentineLayout == true) {
+//     if (kMatrixVertical == false) {
+//       if( y & 0x01) {
+//         // Odd rows run backwards
+//         uint8_t reverseX = (kMatrixWidth - 1) - x;
+//         i = (y * kMatrixWidth) + reverseX;
+//       } else {
+//         // Even rows run forwards
+//         i = (y * kMatrixWidth) + x;
+//       }
+//     } else { // vertical positioning
+//       if ( x & 0x01) {
+//         i = kMatrixHeight * (kMatrixWidth - (x+1))+y;
+//       } else {
+//         i = kMatrixHeight * (kMatrixWidth - x) - (y+1);
+//       }
+//     }
+//   }
   
-  return i;
-}
+//   return i;
+// }
+
+// #define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+// CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
+// CRGB* const leds( leds_plus_safety_pixel + 1);
+
+// uint16_t XYsafe( uint8_t x, uint8_t y)
+// {
+//   if( x >= kMatrixWidth) return -1;
+//   if( y >= kMatrixHeight) return -1;
+//   return XY(x,y);
+// }
 
 
-// Once you've gotten the basics working (AND NOT UNTIL THEN!)
-// here's a helpful technique that can be tricky to set up, but 
-// then helps you avoid the needs for sprinkling array-bound-checking
-// throughout your code.
-//
-// It requires a careful attention to get it set up correctly, but
-// can potentially make your code smaller and faster.
-//
-// Suppose you have an 8 x 5 matrix of 40 LEDs.  Normally, you'd
-// delcare your leds array like this:
-//    CRGB leds[40];
-// But instead of that, declare an LED buffer with one extra pixel in
-// it, "leds_plus_safety_pixel".  Then declare "leds" as a pointer to
-// that array, but starting with the 2nd element (id=1) of that array: 
-//    CRGB leds_with_safety_pixel[41];
-//    CRGB* const leds( leds_plus_safety_pixel + 1);
-// Then you use the "leds" array as you normally would.
-// Now "leds[0..N]" are aliases for "leds_plus_safety_pixel[1..(N+1)]",
-// AND leds[-1] is now a legitimate and safe alias for leds_plus_safety_pixel[0].
-// leds_plus_safety_pixel[0] aka leds[-1] is now your "safety pixel".
-//
-// Now instead of using the XY function above, use the one below, "XYsafe".
-//
-// If the X and Y values are 'in bounds', this function will return an index
-// into the visible led array, same as "XY" does.
-// HOWEVER -- and this is the trick -- if the X or Y values
-// are out of bounds, this function will return an index of -1.
-// And since leds[-1] is actually just an alias for leds_plus_safety_pixel[0],
-// it's a totally safe and legal place to access.  And since the 'safety pixel'
-// falls 'outside' the visible part of the LED array, anything you write 
-// there is hidden from view automatically.
-// Thus, this line of code is totally safe, regardless of the actual size of
-// your matrix:
-//    leds[ XYsafe( random8(), random8() ) ] = CHSV( random8(), 255, 255);
-//
-// The only catch here is that while this makes it safe to read from and
-// write to 'any pixel', there's really only ONE 'safety pixel'.  No matter
-// what out-of-bounds coordinates you write to, you'll really be writing to
-// that one safety pixel.  And if you try to READ from the safety pixel,
-// you'll read whatever was written there last, reglardless of what coordinates
-// were supplied.
-
-#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
-CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
-CRGB* const leds( leds_plus_safety_pixel + 1);
-
-uint16_t XYsafe( uint8_t x, uint8_t y)
-{
-  if( x >= kMatrixWidth) return -1;
-  if( y >= kMatrixHeight) return -1;
-  return XY(x,y);
-}
+// // Demo that USES "XY" follows code below
+// void DrawOneFrame( uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
+// {
+//   uint8_t lineStartHue = startHue8;
+//   for( uint8_t y = 0; y < kMatrixHeight; y++) {
+//     lineStartHue += yHueDelta8;
+//     uint8_t pixelHue = lineStartHue;      
+//     for( uint8_t x = 0; x < kMatrixWidth; x++) {
+//       pixelHue += xHueDelta8;
+//       leds[ XY(x, y)]  = CHSV( pixelHue, 255, 255);
+//     }
+//   }
+// }
 
 
-// Demo that USES "XY" follows code below
-void DrawOneFrame( uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
-{
-  uint8_t lineStartHue = startHue8;
-  for( uint8_t y = 0; y < kMatrixHeight; y++) {
-    lineStartHue += yHueDelta8;
-    uint8_t pixelHue = lineStartHue;      
-    for( uint8_t x = 0; x < kMatrixWidth; x++) {
-      pixelHue += xHueDelta8;
-      leds[ XY(x, y)]  = CHSV( pixelHue, 255, 255);
-    }
-  }
-}
+// void loop()
+// {
+//     uint32_t ms = millis();
+//     int32_t yHueDelta32 = ((int32_t)cos16( ms * (27/1) ) * (350 / kMatrixWidth));
+//     int32_t xHueDelta32 = ((int32_t)cos16( ms * (39/1) ) * (310 / kMatrixHeight));
+//     DrawOneFrame( ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
 
-
-void loop()
-{
-    uint32_t ms = millis();
-    int32_t yHueDelta32 = ((int32_t)cos16( ms * (27/1) ) * (350 / kMatrixWidth));
-    int32_t xHueDelta32 = ((int32_t)cos16( ms * (39/1) ) * (310 / kMatrixHeight));
-    DrawOneFrame( ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
-
-    // maybe setting a random brightness for each pixel could be cool
+//     // maybe setting a random brightness for each pixel could be cool
     
 
 
-    if( ms < 5000 ) {
-      FastLED.setBrightness( scale8( BRIGHTNESS, (ms * 256) / 5000));
-      // FastLED.setBrightness( scale8(random(16, 64), (ms * 256) / 5000)); // thought this might look cool but its just seizure mode (its for brightness of all pixels)
-    } else {
-      FastLED.setBrightness(BRIGHTNESS);
-      // FastLED.setBrightness(random(16, 64)); // thought this might look cool but its just seizure mode (its for brightness of all pixels)
-    }
-    FastLED.show();
-    delay(1000);
-}
+//     if( ms < 5000 ) {
+//       FastLED.setBrightness( scale8( BRIGHTNESS, (ms * 256) / 5000));
+//       // FastLED.setBrightness( scale8(random(16, 64), (ms * 256) / 5000)); // thought this might look cool but its just seizure mode (its for brightness of all pixels)
+//     } else {
+//       FastLED.setBrightness(BRIGHTNESS);
+//       // FastLED.setBrightness(random(16, 64)); // thought this might look cool but its just seizure mode (its for brightness of all pixels)
+//     }
+//     FastLED.show();
+//     // delay(1000);
+// }
 
 
-void setup() {
-  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
-  FastLED.setBrightness( BRIGHTNESS );
-  // randomSeed(analogRead(0));
-}
-
-
-
-
-// #include <Arduino.h>
-// #define FASTLED_INTERNAL
-// // #include <FastLED.h>
-
-
-// // #define NUM_LEDS  256
-// // #define LED_PIN   22 
-
-// // CRGB leds[NUM_LEDS] = {0};  // Initialize the array of leds
-
-// // // FramesPerSecond
-// // //
-// // // Tracks a weighted average to smooth out the values that it calcs as the simple reciprocal
-// // // of the amount of time taken specified by the caller. So 1/3 of a second is 3 fps, and it
-// // // will take up to 10 frames or so to stabilize on that value.
-
-// // double FramesPerSecond(double seconds) {
-// //   static double framesPerSecond;
-// //   framesPerSecond = (framesPerSecond * .9) + (1.0 / seconds * .1);
-// //   return framesPerSecond;
-// // }
-
-// // void setup() {
-// //   pinMode(LED_BUILTIN, OUTPUT);
-// //   pinMode(LED_PIN, OUTPUT);
-
-// //   Serial.begin(115200);
-// //   while (!Serial) {}
-// //   Serial.println("ESP32 Startup");
-
-// //   FastLED.addLeds<WS2811, LED_PIN, GRB>(leds, NUM_LEDS);    // Add our strip to the FastLED library
-// //   FastLED.setBrightness(16);
-
-// // }
-
-// // void loop() {
-// //   bool bLED = 0;
-// //   double fps = 0;
-
-// //   uint8_t hue = 0;
-// //   const uint8_t deltaHue = 8;
-// //   const uint8_t hueDensity = 8;
-
-// //   for (;;)
-// //   {
-// //     bLED = !bLED;
-// //     digitalWrite(LED_BUILTIN, bLED);
-
-// //     double dStart = millis() / 1000.0;
-
-// //     // for (int i = 0; i < NUM_LEDS; i += 5)
-// //     // {
-// //     //   leds[i] = CHSV(hue + (i * hueDensity), 255, 255);
-// //     // }
-// //     fill_rainbow(leds, NUM_LEDS, hue += hueDensity, deltaHue);
-
-// //     FastLED.show();
-// //     Serial.println(fps);
-// //     delay(100);
-  
-// //     double dEnd = millis() / 1000.0;
-// //     fps = FramesPerSecond(dEnd - dStart);
-// //   }
-// // }
-
-
-// /// @file    XYMatrix.ino
-// /// @brief   Demonstrates how to use an XY position helper function with a 2D matrix
-// /// @example XYMatrix.ino
+// void setup() {
+//   FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
+//   FastLED.setBrightness( BRIGHTNESS );
+//   // randomSeed(analogRead(0));
+// }
