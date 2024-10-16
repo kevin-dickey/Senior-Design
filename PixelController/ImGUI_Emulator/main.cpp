e0b00f343e1bcb05fc1eb2adbab332102ae72c45
Nick Vazquez <nmv@iastate.edu>
2024-10-15 20:09:28 -0500

    🐛 Fix Emulator linking & syntax errors

--- PixelController/ImGUI_Emulator/main.cpp
+++ PixelController/ImGUI_Emulator/main.cpp
@@ -7,15 +7,17 @@ int main() {
     Emulator emulator;
     emulator.Init(window, glsl_version);
 
+    auto *clear_color = new ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
         emulator.NewFrame();
-        emulator.Update(new ImVec4(0.5, 0.5, 0.5, 1.0));
+        emulator.Update(clear_color);
         // NOTE: ImGui::Render() will render the frame, but it will not clear the framebuffer.
         // In the ImGui example, the framebuffer is cleared between the ImGui::Render() and [...]RenderDrawData() calls.
-        Window::clearWindow(window);
+        Window::clearWindow(window, clear_color);
         emulator.Render();
 
         // Output the updated GLFW framebuffer to the window.
