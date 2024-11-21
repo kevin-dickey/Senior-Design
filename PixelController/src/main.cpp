16bc8ae44087370df041d7e0a2a0b707c00ffb97
Nick Vazquez <nmv@iastate.edu>
2024-11-21 11:12:32 -0600

    🎨 Remove formatting changes

--- PixelController/src/main.cpp
+++ PixelController/src/main.cpp
@@ -13,7 +13,7 @@ using json = nlohmann::json;
 #define NUM_LEDS_X 16
 #define NUM_LEDS_Y 16
 #define NUM_LEDS 256
-#define MAX_BRIGHTNESS 64 // maximum for FastLED is 255, (don't go higher than like 8 if you don't have a PSU attached)
+#define MAX_BRIGHTNESS 64  // maximum for FastLED is 255, (don't go higher than like 8 if you don't have a PSU attached)
 
 #if USE_EMULATOR
 
@@ -34,8 +34,7 @@ using json = nlohmann::json;
 
 #endif
 
-enum ShiftDirection
-{
+enum ShiftDirection {
     LEFT,
     RIGHT,
     UP,
@@ -126,7 +125,7 @@ int main()
 void fadeToBlack(int duration);
 void fadeToBrightness(int duration, int targetBrightness);
 
-void DrawOneFrame(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8);              // draws rainbow frame
+void DrawOneFrame(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8);  // draws rainbow frame
 void DrawOneFrameReducedBright(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8); // ^ @ half brightness
 
 void parseBitmapData(const char *hexData);
@@ -136,14 +135,13 @@ CRGB hexToCRGB(const char *hex);
 
 void shiftLeds(CRGB leds[], ShiftDirection direction);
 
+// Array of the LEDs. Should be accessed using the XY functions (translation to 2D array, which is not done directly b/c
+//                                                               of different possible layouts of the LEDs (serpentine n such))
 CRGB leds[NUM_LEDS];
 int prevLeds1[NUM_LEDS] = {0};
 int prevLeds2[NUM_LEDS] = {0};
 int prevLeds3[NUM_LEDS] = {0};
 int prevLeds4[NUM_LEDS] = {0};
-
-// Array of the LEDs. Should be accessed using the XY functions (translation to 2D array, which is not done directly b/c
-//                                                               of different possible layouts of the LEDs (serpentine n such)
 
 const char *pumpkin =
     "ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff ffffff "
@@ -203,20 +201,19 @@ namespace std
     }
 }
 
-void setup()
-{
-    Serial.begin(115200);          // for setting up stuff to print to serial monitor
-    delay(3000);                   // delay for 3 seconds to give time to open the serial monitor
-    Serial.println("Starting..."); // print to the serial monitor that the program is starting
+void setup() {
+    Serial.begin(115200);           // for setting up stuff to print to serial monitor
+    delay(3000);                    // delay for 3 seconds to give time to open the serial monitor
+    Serial.println("Starting...");  // print to the serial monitor that the program is starting
 
-    FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050); // setup the LEDs & LED pin for the esp32
-    FastLED.setBrightness(MAX_BRIGHTNESS);                                                        // set the max brightness for the LEDs
-    pinMode(LED_BUILTIN, OUTPUT);                                                                 // setup the built-in LED for the esp32
+    FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);  // setup the LEDs & LED pin for the esp32
+    FastLED.setBrightness(MAX_BRIGHTNESS);                                                         // set the max brightness for the LEDs
+    pinMode(LED_BUILTIN, OUTPUT);                                                                  // setup the built-in LED for the esp32
     fill_solid(leds, NUM_LEDS, CRGB::Black);
     FastLED.show();
     Serial.println("Initialized FastLED...");
 
-    Pair_t sensor_pos{0, 0};
+    Pair_t sensor_pos = {x : 0, y : 0};
     Pair_t size = {10, 10};
 
     std::unique_ptr<Translation_t> translation = std::make_unique<Translation_t>(
@@ -265,16 +262,11 @@ void setup()
 /**
  * Resets every index in prev_sensor_triggered to false except for the specified index, which is set to true.
  */
-void resetTriggerMarkers(int exception)
-{
-    for (int i = 0; i < prev_sensor_triggered.size(); i++)
-    {
-        if (i == exception)
-        {
+void resetTriggerMarkers(int exception) {
+    for (int i = 0; i < prev_sensor_triggered.size(); i++) {
+        if (i == exception) {
             prev_sensor_triggered[i] = true;
-        }
-        else
-        {
+        } else {
             prev_sensor_triggered[i] = false;
         }
     }
@@ -382,31 +374,26 @@ void loop()
     // Kevins scoop:
     //  run(showFrame);
 
-    delay(33); // delay(33): approx 30fps (30.3)
+    delay(33);  // delay(33): approx 30fps (30.3)
 }
 
 // Function to load an 8x8 bitmap from a hex string
 // startX and startY specify the position of the top-right of the bitmap to load in
-void loadHexBitmap(CRGB *leds, const char *bitmap, uint8_t startX, uint8_t startY, int bitmapHeight, int bitmapWidth)
-{
-    for (uint8_t y = 0; y < bitmapHeight; y++)
-    {
-        for (uint8_t x = 0; x < bitmapWidth; x++)
-        {
+void loadHexBitmap(CRGB *leds, const char *bitmap, uint8_t startX, uint8_t startY, int bitmapHeight, int bitmapWidth) {
+    for (uint8_t y = 0; y < bitmapHeight; y++) {
+        for (uint8_t x = 0; x < bitmapWidth; x++) {
             // Calculate the position in the string
-            int index = (y * bitmapWidth + x) * 7; // 6 for color + 1 for space
+            int index = (y * bitmapWidth + x) * 7;  // 6 for color + 1 for space
 
             // Extract the hex color (6 characters)
-            char hexColor[7]; // 6 for color + 1 for null terminator
+            char hexColor[7];  // 6 for color + 1 for null terminator
             strncpy(hexColor, &bitmap[index], 6);
-            hexColor[6] = '\0'; // Null-terminate the string
+            hexColor[6] = '\0';  // Null-terminate the string
 
             // Only load if within bounds
-            if (startX + x < kMatrixWidth && startY + y < kMatrixHeight)
-            {
+            if (startX + x < kMatrixWidth && startY + y < kMatrixHeight) {
                 CRGB color = hexToCRGB(hexColor);
-                if (color != (CRGB::Black))
-                { // black color is interpreted as intending to be transparent
+                if (color != (CRGB::Black)) {  // black color is interpreted as intending to be transparent
                     leds[XY(startX + x, startY + y)] = color;
                 }
             }
@@ -418,15 +405,11 @@ void loadHexBitmap(CRGB *leds, const char *bitmap, uint8_t startX, uint8_t start
 /**
  * DOESN'T WORK. DUNNO WHY!
  */
-void loadByteBitmap(CRGB *leds, const unsigned char *bitmap, uint8_t startX, uint8_t startY, int bitmapHeight, int bitmapWidth)
-{
-    for (uint8_t y = 0; y < bitmapHeight; y++)
-    {
-        for (uint8_t x = 0; x < bitmapWidth; x++)
-        {
+void loadByteBitmap(CRGB *leds, const unsigned char *bitmap, uint8_t startX, uint8_t startY, int bitmapHeight, int bitmapWidth) {
+    for (uint8_t y = 0; y < bitmapHeight; y++) {
+        for (uint8_t x = 0; x < bitmapWidth; x++) {
             // Only draw if within bounds of the LED matrix
-            if ((startX + x) < kMatrixWidth && (startY + y) < kMatrixHeight)
-            {
+            if ((startX + x) < kMatrixWidth && (startY + y) < kMatrixHeight) {
                 // Calculate the index for this pixel (3 bytes per pixel: RGB)
                 int index = (y * bitmapWidth + x) * 3;
 
@@ -435,79 +418,66 @@ void loadByteBitmap(CRGB *leds, const unsigned char *bitmap, uint8_t startX, uin
                 snprintf(hexColor, sizeof(hexColor), "%02x%02x%02x", bitmap[index], bitmap[index + 1], bitmap[index + 2]);
                 hexColor[6] = '\0';
 
-                if (startX + x < kMatrixHeight && startY + y < kMatrixHeight)
-                {
+                if (startX + x < kMatrixHeight && startY + y < kMatrixHeight) {
                     CRGB color = hexToCRGB(hexColor);
-                    if (color != (CRGB::Black))
-                    {
+                    if (color != (CRGB::Black)) {
                         leds[XY(startX + x, startY + y)] = color;
                     }
                 }
             }
         }
     }
-    FastLED.show(); // Display the updated LED matrix
+    FastLED.show();  // Display the updated LED matrix
 }
 
-void shiftLeds(CRGB leds[], ShiftDirection direction)
-{
+void shiftLeds(CRGB leds[], ShiftDirection direction) {
     CRGB temp[NUM_LEDS_X * NUM_LEDS_Y];
 
     // Copy current state to temp array
-    for (int i = 0; i < NUM_LEDS_X * NUM_LEDS_Y; i++)
-    {
+    for (int i = 0; i < NUM_LEDS_X * NUM_LEDS_Y; i++) {
         temp[i] = leds[i];
     }
 
-    switch (direction)
-    {
-    case RIGHT: // if you look closely right and left might look flipped, and you're right!
-                // don't ask me why, it just works :)
-        for (int y = 0; y < NUM_LEDS_Y; y++)
-        {
-            for (int x = 0; x < NUM_LEDS_X; x++)
-            {
-                int newX = (x - 1 + NUM_LEDS_X) % NUM_LEDS_X;
-                leds[XY(newX, y)] = temp[XY(x, y)];
+    switch (direction) {
+        case RIGHT:  // if you look closely right and left might look flipped, and you're right!
+                     // don't ask me why, it just works :)
+            for (int y = 0; y < NUM_LEDS_Y; y++) {
+                for (int x = 0; x < NUM_LEDS_X; x++) {
+                    int newX = (x - 1 + NUM_LEDS_X) % NUM_LEDS_X;
+                    leds[XY(newX, y)] = temp[XY(x, y)];
+                }
             }
-        }
-        break;
+            break;
 
-    case LEFT:
-        for (int y = 0; y < NUM_LEDS_Y; y++)
-        {
-            for (int x = 0; x < NUM_LEDS_X; x++)
-            {
-                int newX = (x + 1) % NUM_LEDS_X;
-                leds[XY(newX, y)] = temp[XY(x, y)];
+        case LEFT:
+            for (int y = 0; y < NUM_LEDS_Y; y++) {
+                for (int x = 0; x < NUM_LEDS_X; x++) {
+                    int newX = (x + 1) % NUM_LEDS_X;
+                    leds[XY(newX, y)] = temp[XY(x, y)];
+                }
             }
-        }
-        break;
+            break;
 
-    case UP:
-        for (int x = 0; x < NUM_LEDS_X; x++)
-        {
-            for (int y = 0; y < NUM_LEDS_Y; y++)
-            {
-                int newY = (y - 1 + NUM_LEDS_Y) % NUM_LEDS_Y;
-                leds[XY(x, newY)] = temp[XY(x, y)];
+        case UP:
+            for (int x = 0; x < NUM_LEDS_X; x++) {
+                for (int y = 0; y < NUM_LEDS_Y; y++) {
+                    int newY = (y - 1 + NUM_LEDS_Y) % NUM_LEDS_Y;
+                    leds[XY(x, newY)] = temp[XY(x, y)];
+                }
             }
-        }
-        break;
+            break;
 
-    case DOWN:
-        for (int x = 0; x < NUM_LEDS_X; x++)
-        {
-            for (int y = 0; y < NUM_LEDS_Y; y++)
-            {
-                int newY = (y + 1) % NUM_LEDS_Y;
-                leds[XY(x, newY)] = temp[XY(x, y)];
+        case DOWN:
+            for (int x = 0; x < NUM_LEDS_X; x++) {
+                for (int y = 0; y < NUM_LEDS_Y; y++) {
+                    int newY = (y + 1) % NUM_LEDS_Y;
+                    leds[XY(x, newY)] = temp[XY(x, y)];
+                }
             }
-        }
-        break;
+            break;
     }
 
-    FastLED.show(); // Update the LED display
+    FastLED.show();  // Update the LED display
 }
 
 /**
@@ -518,16 +488,14 @@ void shiftLeds(CRGB leds[], ShiftDirection direction)
  * If you want to use it on a specific subset of LEDs, will have to provide
  * that subset as well as somehow keeping track of what the LEDs previously were.
  */
-void fadeToBlack(int duration)
-{
+void fadeToBlack(int duration) {
     uint8_t initialBrightness = FastLED.getBrightness();
     if (initialBrightness == 0)
         return;
 
     int updatesPerSec = initialBrightness / duration;
 
-    for (int i = initialBrightness; i > 0; i--)
-    {
+    for (int i = initialBrightness; i > 0; i--) {
         FastLED.setBrightness(i);
         FastLED.show();
         delay(1000 / updatesPerSec);
@@ -549,41 +517,34 @@ void fadeToBlack(int duration)
  * If you want to use it on a specific subset of LEDs, will have to provide
  * that subset as well as somehow keeping track of what the LEDs previously were.
  */
-void fadeToBrightness(int duration, int targetBrightness)
-{
+void fadeToBrightness(int duration, int targetBrightness) {
     uint8_t curBrightness = FastLED.getBrightness();
     if (curBrightness == targetBrightness)
         return;
 
     int updatesPerSec;
-    if (targetBrightness > curBrightness)
-    { // increase brightness to target
+    if (targetBrightness > curBrightness) {  // increase brightness to target
         updatesPerSec = (targetBrightness - curBrightness) / duration;
-        for (int i = curBrightness; i < targetBrightness; i++)
-        {
+        for (int i = curBrightness; i < targetBrightness; i++) {
             FastLED.setBrightness(i);
             FastLED.show();
             delay(1000 / updatesPerSec);
         }
-    }
-    else
-    { // decrease brightness to target
+    } else {  // decrease brightness to target
         updatesPerSec = (curBrightness - targetBrightness) / duration;
-        for (int i = curBrightness; i > targetBrightness; i--)
-        {
+        for (int i = curBrightness; i > targetBrightness; i--) {
             FastLED.setBrightness(i);
             FastLED.show();
             delay(1000 / updatesPerSec);
         }
     }
 
-    FastLED.setBrightness(targetBrightness); // just in case it doesnt fully work lol
+    FastLED.setBrightness(targetBrightness);  // just in case it doesnt fully work lol
     FastLED.show();
 }
 
 // Function to convert a 6-character hex string to CRGB
-CRGB hexToCRGB(const char *hex)
-{
+CRGB hexToCRGB(const char *hex) {
     uint8_t r = strtol(std::string(hex, 2).c_str(), NULL, 16);
     uint8_t g = strtol(std::string(hex + 2, 2).c_str(), NULL, 16);
     uint8_t b = strtol(std::string(hex + 4, 2).c_str(), NULL, 16);
@@ -591,28 +552,22 @@ CRGB hexToCRGB(const char *hex)
 }
 
 // Function to parse the bitmap data from a hex string
-void parseBitmapData(const char *hexData)
-{
+void parseBitmapData(const char *hexData) {
     int index = 0;
-    while (*hexData)
-    {
+    while (*hexData) {
         // Skip spaces
-        if (*hexData == ' ')
-        {
+        if (*hexData == ' ') {
             hexData++;
             continue;
         }
 
         // Convert the next 6 characters to CRGB and store in the leds array
-        if (index < NUM_LEDS)
-        {
+        if (index < NUM_LEDS) {
             leds[index] = hexToCRGB(hexData);
-            hexData += 6; // Move to the next color
+            hexData += 6;  // Move to the next color
             index++;
-        }
-        else
-        {
-            break; // Avoid exceeding the array size
+        } else {
+            break;  // Avoid exceeding the array size
         }
     }
 }
@@ -620,15 +575,12 @@ void parseBitmapData(const char *hexData)
 /**
  * Draws a single frame of the rainbow effect
  */
-void DrawOneFrame(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
-{
+void DrawOneFrame(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8) {
     uint8_t lineStartHue = startHue8;
-    for (uint8_t y = 0; y < kMatrixHeight; y++)
-    {
+    for (uint8_t y = 0; y < kMatrixHeight; y++) {
         lineStartHue += yHueDelta8;
         uint8_t pixelHue = lineStartHue;
-        for (uint8_t x = 0; x < kMatrixWidth; x++)
-        {
+        for (uint8_t x = 0; x < kMatrixWidth; x++) {
             pixelHue += xHueDelta8;
             leds[XY(x, y)] = CHSV(pixelHue, 255, 255);
         }
@@ -638,15 +590,12 @@ void DrawOneFrame(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
 /**
  * Draws a single frame of the rainbow effect
  */
-void DrawOneFrameReducedBright(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
-{
+void DrawOneFrameReducedBright(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8) {
     uint8_t lineStartHue = startHue8;
-    for (uint8_t y = 0; y < kMatrixHeight; y++)
-    {
+    for (uint8_t y = 0; y < kMatrixHeight; y++) {
         lineStartHue += yHueDelta8;
         uint8_t pixelHue = lineStartHue;
-        for (uint8_t x = 0; x < kMatrixWidth; x++)
-        {
+        for (uint8_t x = 0; x < kMatrixWidth; x++) {
             pixelHue += xHueDelta8;
             leds[XY(x, y)] = CHSV(pixelHue * 5 / 6, 255 * 5 / 6, 255 * 5 / 6);
         }
