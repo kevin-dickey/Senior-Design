235c7418695430ea003c75db827729e5ecfd6651
Nick Vazquez <nmv@iastate.edu>
2024-11-22 09:02:58 -0600

    🔥 Remove unnecessary main functions

--- PixelController/src/main.cpp
+++ PixelController/src/main.cpp
@@ -33,25 +33,15 @@ using json = nlohmann::json;
 
 #endif
 
-enum ShiftDirection
-{
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-};
-
 FileManager *fileManager;
 Show_t show;
 
-// Function prototypes
+#if USE_EMULATOR
 void loadExampleImages(const std::vector<std::string *> &image_paths,
                        std::vector<ImageProcessing::ImageData_t> &out_loadedImages);
 
 void resizeImages(const std::vector<ImageProcessing::ImageData_t> &loadedImages, GridLayout *gridLayout,
                   std::vector<ImageProcessing::ImageData_t> &resizedImages);
-
-#if USE_EMULATOR
 
 int main()
 {
@@ -89,27 +79,77 @@ int main()
     return 0;
 }
 
+void loadExampleImages(const std::vector<std::string *> &image_paths,
+                       std::vector<ImageProcessing::ImageData_t> &out_loadedImages)
+{
+    // Clear the loaded images
+    out_loadedImages.clear();
+
+    for (const auto &i : image_paths)
+    {
+        // Load the imagePath
+        std::string imagePath = *i;
+
+        int ok, width, height, channels;
+        ok = ImageProcessing::get_image_dimensions(imagePath.c_str(), &width, &height, &channels);
+        if (!ok)
+        {
+            std::cerr << "Error getting imagePath dimensions" << std::endl;
+            return;
+        }
+
+        unsigned char *data = ImageProcessing::load_image(imagePath.c_str(), &width, &height, &channels);
+        if (data == nullptr)
+        {
+            std::cerr << "Error loading imagePath: " << imagePath << std::endl;
+            return;
+        }
+
+        std::cout << "Image Filename: " << imagePath << std::endl;
+        std::cout << "Image Width: " << width << std::endl;
+        std::cout << "Image Height: " << height << std::endl;
+        std::cout << "Image Channels: " << channels << std::endl;
+
+        // Store the loaded image
+        out_loadedImages.push_back({data, width, height, channels});
+    }
+
+    std::cout << "Loaded " << out_loadedImages.size() << " images" << std::endl;
+}
+
+void resizeImages(const std::vector<ImageProcessing::ImageData_t> &loadedImages, GridLayout *gridLayout,
+                  std::vector<ImageProcessing::ImageData_t> &resizedImages)
+{
+    // Resize the loaded images to the grid layout size
+    for (auto &i : loadedImages)
+    {
+        int resize_width = gridLayout->width;
+        int resize_height = gridLayout->height;
+
+        unsigned char *resizedImage = ImageProcessing::resize_image(i.data, i.width, i.height, i.channels,
+                                                                    resize_width, resize_height);
+        if (resizedImage == nullptr)
+        {
+            std::cerr << "Error resizing image" << std::endl;
+            return;
+        }
+
+        // Store the resized image
+        resizedImages.push_back({resizedImage, resize_width, resize_height, i.channels});
+    }
+}
+
 #else
 
 /**
  * MARK: Setup
  */
 
-void fadeToBlack(int duration);
-void fadeToBrightness(int duration, int targetBrightness);
-
-void DrawOneFrame(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8);              // draws rainbow frame
-void DrawOneFrameReducedBright(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8); // ^ @ half brightness
-
 void bufferToCRGBArray(unsigned char *buffer, int imgWidth, int imgHeight, int imgChannels, CRGB *leds, int matrixWidth, int matrixHeight);
 void fillRemainingPixels(CRGB *leds, int matrixWidth, int matrixHeight, CRGB backgroundColor);
 
-void parseBitmapData(const char *hexData);
 void loadHexBitmap(CRGB *leds, const char *bitmap, uint8_t startX, uint8_t startY, int bitmapHeight, int bitmapWidth);
-void loadByteBitmap(CRGB *leds, const unsigned char *bitmap, uint8_t startX, uint8_t startY, int bitmapHeight, int bitmapWidth);
 CRGB hexToCRGB(const char *hex);
-
-void shiftLeds(CRGB leds[], ShiftDirection direction);
 
 // Array of the LEDs. Should be accessed using the XY functions (translation to 2D array, which is not done directly b/c
 //                                                               of different possible layouts of the LEDs (serpentine n such))
@@ -270,172 +310,6 @@ void loadHexBitmap(CRGB *leds, const char *bitmap, uint8_t startX, uint8_t start
     FastLED.show();
 }
 
-/**
- * DOESN'T WORK. DUNNO WHY!
- */
-void loadByteBitmap(CRGB *leds, const unsigned char *bitmap, uint8_t startX, uint8_t startY, int bitmapHeight, int bitmapWidth)
-{
-    for (uint8_t y = 0; y < bitmapHeight; y++)
-    {
-        for (uint8_t x = 0; x < bitmapWidth; x++)
-        {
-            // Only draw if within bounds of the LED matrix
-            if ((startX + x) < kMatrixWidth && (startY + y) < kMatrixHeight)
-            {
-                // Calculate the index for this pixel (3 bytes per pixel: RGB)
-                int index = (y * bitmapWidth + x) * 3;
-
-                // Extract RGB values from the hex array
-                char hexColor[7];
-                snprintf(hexColor, sizeof(hexColor), "%02x%02x%02x", bitmap[index], bitmap[index + 1], bitmap[index + 2]);
-                hexColor[6] = '\0';
-
-                if (startX + x < kMatrixHeight && startY + y < kMatrixHeight)
-                {
-                    CRGB color = hexToCRGB(hexColor);
-                    if (color != (CRGB::Black))
-                    {
-                        leds[XY(startX + x, startY + y)] = color;
-                    }
-                }
-            }
-        }
-    }
-    FastLED.show(); // Display the updated LED matrix
-}
-
-void shiftLeds(CRGB leds[], ShiftDirection direction)
-{
-    CRGB temp[NUM_LEDS_X * NUM_LEDS_Y];
-
-    // Copy current state to temp array
-    for (int i = 0; i < NUM_LEDS_X * NUM_LEDS_Y; i++)
-    {
-        temp[i] = leds[i];
-    }
-
-    switch (direction)
-    {
-    case RIGHT: // if you look closely right and left might look flipped, and you're right!
-                // don't ask me why, it just works :)
-        for (int y = 0; y < NUM_LEDS_Y; y++)
-        {
-            for (int x = 0; x < NUM_LEDS_X; x++)
-            {
-                int newX = (x - 1 + NUM_LEDS_X) % NUM_LEDS_X;
-                leds[XY(newX, y)] = temp[XY(x, y)];
-            }
-        }
-        break;
-
-    case LEFT:
-        for (int y = 0; y < NUM_LEDS_Y; y++)
-        {
-            for (int x = 0; x < NUM_LEDS_X; x++)
-            {
-                int newX = (x + 1) % NUM_LEDS_X;
-                leds[XY(newX, y)] = temp[XY(x, y)];
-            }
-        }
-        break;
-
-    case UP:
-        for (int x = 0; x < NUM_LEDS_X; x++)
-        {
-            for (int y = 0; y < NUM_LEDS_Y; y++)
-            {
-                int newY = (y - 1 + NUM_LEDS_Y) % NUM_LEDS_Y;
-                leds[XY(x, newY)] = temp[XY(x, y)];
-            }
-        }
-        break;
-
-    case DOWN:
-        for (int x = 0; x < NUM_LEDS_X; x++)
-        {
-            for (int y = 0; y < NUM_LEDS_Y; y++)
-            {
-                int newY = (y + 1) % NUM_LEDS_Y;
-                leds[XY(x, newY)] = temp[XY(x, y)];
-            }
-        }
-        break;
-    }
-
-    FastLED.show(); // Update the LED display
-}
-
-/**
- * duration is given in seconds.
- *
- * Uses FastLED's builtin for setting brightness, so it'll modify every LED.
- *
- * If you want to use it on a specific subset of LEDs, will have to provide
- * that subset as well as somehow keeping track of what the LEDs previously were.
- */
-void fadeToBlack(int duration)
-{
-    uint8_t initialBrightness = FastLED.getBrightness();
-    if (initialBrightness == 0)
-        return;
-
-    int updatesPerSec = initialBrightness / duration;
-
-    for (int i = initialBrightness; i > 0; i--)
-    {
-        FastLED.setBrightness(i);
-        FastLED.show();
-        delay(1000 / updatesPerSec);
-    }
-
-    // Ensure the brightness is fully set to 0 at the end
-    FastLED.setBrightness(0);
-    FastLED.show();
-}
-
-/**
- * Slowly and "smoothly" transitions the brightness of all LEDs to the targetBrightness over the given duration.
- *
- * duration is given in seconds.
- * targetBrightness should generally not be set beyond 32 (64 likely maximum for safety/consistent power delivery)
- *
- * Uses FastLED's builtin for setting brightness, so it'll modify every LED.
- *
- * If you want to use it on a specific subset of LEDs, will have to provide
- * that subset as well as somehow keeping track of what the LEDs previously were.
- */
-void fadeToBrightness(int duration, int targetBrightness)
-{
-    uint8_t curBrightness = FastLED.getBrightness();
-    if (curBrightness == targetBrightness)
-        return;
-
-    int updatesPerSec;
-    if (targetBrightness > curBrightness)
-    { // increase brightness to target
-        updatesPerSec = (targetBrightness - curBrightness) / duration;
-        for (int i = curBrightness; i < targetBrightness; i++)
-        {
-            FastLED.setBrightness(i);
-            FastLED.show();
-            delay(1000 / updatesPerSec);
-        }
-    }
-    else
-    { // decrease brightness to target
-        updatesPerSec = (curBrightness - targetBrightness) / duration;
-        for (int i = curBrightness; i > targetBrightness; i--)
-        {
-            FastLED.setBrightness(i);
-            FastLED.show();
-            delay(1000 / updatesPerSec);
-        }
-    }
-
-    FastLED.setBrightness(targetBrightness); // just in case it doesnt fully work lol
-    FastLED.show();
-}
-
 // Function to convert a 6-character hex string to CRGB
 CRGB hexToCRGB(const char *hex)
 {
@@ -443,69 +317,6 @@ CRGB hexToCRGB(const char *hex)
     uint8_t g = strtol(std::string(hex + 2, 2).c_str(), NULL, 16);
     uint8_t b = strtol(std::string(hex + 4, 2).c_str(), NULL, 16);
     return CRGB(r, g, b);
-}
-
-// Function to parse the bitmap data from a hex string
-void parseBitmapData(const char *hexData)
-{
-    int index = 0;
-    while (*hexData)
-    {
-        // Skip spaces
-        if (*hexData == ' ')
-        {
-            hexData++;
-            continue;
-        }
-
-        // Convert the next 6 characters to CRGB and store in the leds array
-        if (index < NUM_LEDS)
-        {
-            leds[index] = hexToCRGB(hexData);
-            hexData += 6; // Move to the next color
-            index++;
-        }
-        else
-        {
-            break; // Avoid exceeding the array size
-        }
-    }
-}
-
-/**
- * Draws a single frame of the rainbow effect
- */
-void DrawOneFrame(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
-{
-    uint8_t lineStartHue = startHue8;
-    for (uint8_t y = 0; y < kMatrixHeight; y++)
-    {
-        lineStartHue += yHueDelta8;
-        uint8_t pixelHue = lineStartHue;
-        for (uint8_t x = 0; x < kMatrixWidth; x++)
-        {
-            pixelHue += xHueDelta8;
-            leds[XY(x, y)] = CHSV(pixelHue, 255, 255);
-        }
-    }
-}
-
-/**
- * Draws a single frame of the rainbow effect
- */
-void DrawOneFrameReducedBright(uint8_t startHue8, int8_t yHueDelta8, int8_t xHueDelta8)
-{
-    uint8_t lineStartHue = startHue8;
-    for (uint8_t y = 0; y < kMatrixHeight; y++)
-    {
-        lineStartHue += yHueDelta8;
-        uint8_t pixelHue = lineStartHue;
-        for (uint8_t x = 0; x < kMatrixWidth; x++)
-        {
-            pixelHue += xHueDelta8;
-            leds[XY(x, y)] = CHSV(pixelHue * 5 / 6, 255 * 5 / 6, 255 * 5 / 6);
-        }
-    }
 }
 
 void bufferToCRGBArray(unsigned char *buffer, int imgWidth, int imgHeight, int imgChannels, CRGB *leds, int matrixWidth, int matrixHeight)
@@ -547,63 +358,3 @@ void fillRemainingPixels(CRGB *leds, int matrixWidth, int matrixHeight, CRGB bac
     }
 }
 #endif
-
-void loadExampleImages(const std::vector<std::string *> &image_paths,
-                       std::vector<ImageProcessing::ImageData_t> &out_loadedImages)
-{
-    // Clear the loaded images
-    out_loadedImages.clear();
-
-    for (const auto &i : image_paths)
-    {
-        // Load the imagePath
-        std::string imagePath = *i;
-
-        int ok, width, height, channels;
-        ok = ImageProcessing::get_image_dimensions(imagePath.c_str(), &width, &height, &channels);
-        if (!ok)
-        {
-            std::cerr << "Error getting imagePath dimensions" << std::endl;
-            return;
-        }
-
-        unsigned char *data = ImageProcessing::load_image(imagePath.c_str(), &width, &height, &channels);
-        if (data == nullptr)
-        {
-            std::cerr << "Error loading imagePath: " << imagePath << std::endl;
-            return;
-        }
-
-        std::cout << "Image Filename: " << imagePath << std::endl;
-        std::cout << "Image Width: " << width << std::endl;
-        std::cout << "Image Height: " << height << std::endl;
-        std::cout << "Image Channels: " << channels << std::endl;
-
-        // Store the loaded image
-        out_loadedImages.push_back({data, width, height, channels});
-    }
-
-    std::cout << "Loaded " << out_loadedImages.size() << " images" << std::endl;
-}
-
-void resizeImages(const std::vector<ImageProcessing::ImageData_t> &loadedImages, GridLayout *gridLayout,
-                  std::vector<ImageProcessing::ImageData_t> &resizedImages)
-{
-    // Resize the loaded images to the grid layout size
-    for (auto &i : loadedImages)
-    {
-        int resize_width = gridLayout->width;
-        int resize_height = gridLayout->height;
-
-        unsigned char *resizedImage = ImageProcessing::resize_image(i.data, i.width, i.height, i.channels,
-                                                                    resize_width, resize_height);
-        if (resizedImage == nullptr)
-        {
-            std::cerr << "Error resizing image" << std::endl;
-            return;
-        }
-
-        // Store the resized image
-        resizedImages.push_back({resizedImage, resize_width, resize_height, i.channels});
-    }
-}
